cdc1bc3ff450e43ab2e677fc2904a64efa24c537
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-07-11 19:29:34 -0700

    added random color generator

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
 var body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
+	body.style.background =
+	"linear-gradient(to right, "
+	+ color1.value
+	+ ", "
+	+ color2.value
 	+ ")";
 
 	css.textContent = body.style.background + ";";
 }
+
+const setBg = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  body.style.background = "#" + randomColor;
+	// color.innerHTML = "#" + randomColor;
+	css.textContent = body.style.background + ";";
+}
+
+genNew.addEventListener("click", setBg);
+setBg();
 
 color1.addEventListener("input", setGradient);
 
